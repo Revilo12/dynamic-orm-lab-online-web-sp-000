9a975866fd83fce19be66d1b189177447e1966fd
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-09 11:49:43 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -64,7 +64,7 @@ class InteractiveRecord
   def self.find_by(option)
     sql  = <<-SQL
         SELECT * FROM #{table_name}
-        WHERE #{option.keys[0].to_s} = #{option.values[0].to_s unless option.values[0].class == integer}
+        WHERE #{option.keys[0].to_s} = #{option.values[0].to_s unless option.values[0].class == Integer}
         SQL
     DB[:conn].execute(sql)
   end
