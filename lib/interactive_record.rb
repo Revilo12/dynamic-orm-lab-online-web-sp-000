ad6354463d6376629d070520362db0228875d144
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-09 11:33:52 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,7 +54,7 @@ class InteractiveRecord
 
   def self.find_by_name(name)
     sql  = <<-SQL
-        SELECT * FROM #{table_name_for_insert}
+        SELECT * FROM #{table_name}
         WHERE name = ?
         SQL
     Student.new(DB[:conn].execute(sql, name))
