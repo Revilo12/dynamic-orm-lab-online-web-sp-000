26c49484bb791005b9911042f57151f912cc42b5
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-09 11:35:40 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -57,7 +57,7 @@ class InteractiveRecord
         SELECT * FROM #{table_name}
         WHERE name = ?
         SQL
-    DB[:conn].execute(sql, name)[0]
+    DB[:conn].execute(sql, name)
   end
 
 end
